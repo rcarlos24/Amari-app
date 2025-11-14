fc67eb5739f0d36ed46156e951e4d0ca5e1b29e4
rcarlos24 <robertoc.sanchez24@gmail.com>
2025-11-14 15:35:39 -0500

    feat: add library of app pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { BienestarComponent } from './bienestar/bienestar.component';
 import { MotivacionComponent } from './motivacion/motivacion.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DiarioComponent } from './diario/diario.component';
 import { ComunidadComponent } from './comunidad/comunidad.component';
 import { SeguridadComponent } from './seguridad/seguridad.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,14 @@ import { SeguridadComponent } from './seguridad/seguridad.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/seguridad/seguridad.component.ts
+++ src/app/seguridad/seguridad.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-seguridad',
   templateUrl: './seguridad.component.html',
   styleUrls: ['./seguridad.component.css']
 })
-export class SeguridadComponent {
+export class SeguridadComponent implements OnInit {
+
+  showModal = false;
+  contactForm!: FormGroup;
+  contacts: any[] = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+  this.contactForm = this.fb.group({
+    name: ['', [Validators.required]],
+    phone: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/^9\d{8}$/)
+      ]
+    ]
+  });
+
+  this.loadContacts();
+}
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  loadContacts() {
+  const saved = localStorage.getItem('contacts');
+  this.contacts = saved ? JSON.parse(saved) : [];
+}
+
+  saveContact() {
+  if (this.contactForm.invalid) return;
+
+  const newContact = this.contactForm.value;
+
+  // Obtener contactos previos
+  const saved = localStorage.getItem('contacts');
+  let contacts = saved ? JSON.parse(saved) : [];
+
+  // Agregar el nuevo
+  contacts.push(newContact);
+
+  // Guardar nuevamente
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+
+  // Actualizar lista en pantalla
+  this.loadContacts();
+
+  this.closeModal();
+}
+
 
 }
