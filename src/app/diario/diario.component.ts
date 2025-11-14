d2ff9f85b22ec10a8a747ffbdfb6220a3b35cc15
rcarlos24 <robertoc.sanchez24@gmail.com>
2025-11-14 16:33:29 -0500

    feat: se agrego funcionalidad a mi diario

--- src/app/diario/diario.component.ts
+++ src/app/diario/diario.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diario',
   templateUrl: './diario.component.html',
-  styleUrls: ['./diario.component.css']
+  styleUrls: ['./diario.component.css'],
 })
-export class DiarioComponent {
+export class DiarioComponent implements OnInit {
+  entradaHoy: string = '';
+  listaEntradas: { dia: number; texto: string }[] = [];
 
+  ngOnInit() {
+  const data = localStorage.getItem('entradas');
+
+  this.listaEntradas = data ? JSON.parse(data) : [];
+}
+
+guardar() {
+  if (!this.entradaHoy.trim()) return;
+
+   const hoy = new Date();
+  const dia = hoy.getDate();
+
+  const nuevaEntrada = {
+    dia,
+    texto: this.entradaHoy
+  };
+
+  this.listaEntradas.push(nuevaEntrada);
+
+  // Guardar lista completa en localStorage
+  localStorage.setItem('entradas', JSON.stringify(this.listaEntradas));
+
+  this.entradaHoy = ''; // limpiar textarea
+}
 }
